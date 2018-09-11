c09b3b2da7a210ea3719314a3ac09b1fdee29b64
wangkj <wangkuijuan@126.com>
2018-09-11 14:25:27 +0800

    add logout when close window

--- src/main/java/io/github/biezhi/wechat/api/WeChatApiImpl.java
+++ src/main/java/io/github/biezhi/wechat/api/WeChatApiImpl.java
@@ -441,19 +441,18 @@ public class WeChatApiImpl implements WeChatApi {
      */
     @Override
     public void logout() {
-        bot.api().login(true);
-//        if (bot.isRunning()) {
-//            String url = String.format("%s/webwxlogout", bot.session().getUrl());
-//            this.client.send(new StringRequest(url)
-//                    .add("redirect", 1)
-//                    .add("type", 1)
-//                    .add("sKey", bot.session().getSKey()));
-//            bot.setRunning(false);
-//        }
-//        this.logging = false;
-//        this.client.cookies().clear();
-//        String file = bot.config().assetsDir() + "/login.json";
-//        new File(file).delete();
+        if (bot.isRunning()) {
+            String url = String.format("%s/webwxlogout", bot.session().getUrl());
+            this.client.send(new StringRequest(url)
+                    .add("redirect", 1)
+                    .add("type", 1)
+                    .add("sKey", bot.session().getSKey()));
+            bot.setRunning(false);
+        }
+        this.logging = false;
+        this.client.cookies().clear();
+        String file = bot.config().assetsDir() + "/login.json";
+        new File(file).delete();
     }
 
     /**
